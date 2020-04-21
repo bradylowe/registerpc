3089547f1894220028a24d407ee07edbee5d97a6
Brady Lowe <lowebra2@isu.edu>
2020-04-21 10:26:52 -0600

    Updated rotate functions to include center. Wrote some translate functions. Changed buildImage function to no longer depend on PIL or BytesIO, now using qimage2ndarray to go between numpy and QImage.

--- registerpc/Room.py
+++ registerpc/Room.py
@@ -2,12 +2,11 @@
 from registerpc.pointcloud.PointCloud import PointCloud
 from registerpc.pointcloud.Voxelize import VoxelGrid
 from registerpc.label_file import LabelFile, LabelFileError
-import PIL
-from io import BytesIO
 from tqdm import tqdm
 from qtpy import QtCore, QtGui
 import numpy as np
 from collections import defaultdict
+import qimage2ndarray
 
 
 class Room:
@@ -28,8 +27,8 @@ class Room:
         points = self.pointcloud.points[['x', 'y', 'z']].values
         self.min_point, self.max_point = points.min(axis=0), points.max(axis=0)
         self.min_idx, self.max_idx = (self.min_point / self.mesh).astype(int), (self.max_point / self.mesh).astype(int)
-        self.offset = QtCore.QPointF(self.min_point[0], self.min_point[1])
         self.min_slice, self.max_slice = int(self.min_point[2] / thickness) - 1, int(self.max_point[2] / thickness)
+        self.offset = QtCore.QPointF(self.min_point[0], self.min_point[1])
         self.slices = defaultdict(list)
         for idx in range(self.min_slice, self.max_slice + 1):
             vals = self.pointcloud.points['z']
@@ -39,23 +38,40 @@ class Room:
     def rotate(self, angle, center=None, indices=None):
         if center is None:
             center = self.center
-        self.rotate_annotations(angle)
-        self.rotate_points(angle, indices)
+        self.rotate_annotations(angle, center)
+        self.rotate_points(angle, center, indices)
 
-    def rotate_annotations(self, angle):
+    def rotate_annotations(self, angle, center):
         pass
 
-    def rotate_points(self, angle, indices=None):
+    def rotate_points(self, angle, center, indices=None):
         angle = np.radians(angle)
         c, s = np.cos(angle), np.sin(angle)
         rot = np.array(((c, s), (-s, c)))
         if indices is None:
+            self.pointcloud.points[['x', 'y']] -= center
             self.pointcloud.points[['x', 'y']] = np.dot(self.pointcloud.points[['x', 'y']].values, rot)
+            self.pointcloud.points[['x', 'y']] += center
         else:
-            self.pointcloud.points.loc[indices][['x', 'y']] = np.dot(self.pointcloud.points.loc[indices][['x', 'y']].values, rot)
+            self.pointcloud.points.iloc[indices][['x', 'y']] -= center
+            self.pointcloud.points.iloc[indices][['x', 'y']] = np.dot(self.pointcloud.points.iloc[indices][['x', 'y']].values, rot)
+            self.pointcloud.points.iloc[indices][['x', 'y']] += center
+
+    def translate(self, delta, indices=None):
+        self.translate_annotations(delta)
+        self.translate_points(delta, indices)
+
+    def translate_annotations(self, delta):
+        pass
+
+    def translate_points(self, delta, indices=None):
+        if indices is None:
+            self.pointcloud.points[['x', 'y']] += delta
+        else:
+            self.pointcloud.points.loc[indices][['x', 'y']] += delta
 
     def slice(self, idx):
-        return self.pointcloud.points.loc[self.slices[idx]][['x', 'y', 'z']].values
+        return self.pointcloud.points.iloc[self.slices[idx]][['x', 'y', 'z']].values
 
     def save(self, pointFile, labelFile, shapes, otherData=None):
         self.pointcloud.write(pointFile, overwrite=True)
@@ -64,11 +80,10 @@ class Room:
     def buildImage(self, idx):
         vg = VoxelGrid(self.slice(idx), (self.mesh, self.mesh, 100000.), (0.0, 0.0, -10000.))
         bitmap = vg.bitmapFromSlice(max=255, min_idx=self.min_idx, max_idx=self.max_idx)
-        img = PIL.Image.fromarray(np.asarray(bitmap, dtype="uint8"))
-        buff = BytesIO()
-        img.save(buff, format="JPEG")
-        buff.seek(0)
-        return QtGui.QImage.fromData(buff.read())
+        image = qimage2ndarray.array2qimage(bitmap)
+        imview = qimage2ndarray.rgb_view(image)
+        imview[:, :, 1] = 0
+        return image
 
     @property
     def points(self):
