759e9b90c6ac4c21f3ba5d99a94b0db0b5b455d5
Brady Lowe <lowebra2@isu.edu>
2020-04-29 11:58:24 -0600

    Cleaned up writing laz file code. Fixed bug for writing new las file.

--- registerpc/pointcloud/PointCloud.py
+++ registerpc/pointcloud/PointCloud.py
@@ -1,4 +1,5 @@
 from laspy.file import File
+from laspy.header import Header
 import open3d as o3d
 import numpy as np
 import pandas as pd
@@ -201,8 +202,10 @@ class PointCloud:
             mask = self.select(indices, highlighted, showing)
         if points is None:
             points = self.points.loc[mask.bools]
-        if filename.endswith('.las') or filename.endswith('.laz'):
+        if filename.endswith('.las'):
             self.__write_las_file(filename, points)
+        elif filename.endswith('.laz'):
+            self.__write_laz_file(filename, points)
         elif filename.endswith('.ply'):
             self.__write_ply_file(filename, points)
         elif filename.endswith('.pcd'):
@@ -214,10 +217,16 @@ class PointCloud:
             return
         print('Wrote %d points to %s' % (len(points), filename))
 
+    def __write_laz_file(self, filename, points):
+        self.__write_las_file('TEMPORARY.las', points)
+        self.__zip_las('TEMPORARY.las', filename)
+        os.system('rm TEMPORARY.las')
+
     def __write_las_file(self, filename, points):
-        if filename.endswith('.laz'):
-            orig_filename = filename
-            filename = 'TEMPORARY.las'
+        if self.las_header is None:
+            self.las_header = Header()
+            self.las_header.offset = [0.0, 0.0, 0.0]
+            self.las_header.scale = [0.0001, 0.0001, 0.0001]
         if self.las_header.data_format_id < 2:
             self.las_header.data_format_id = 2
         with File(filename, self.las_header, mode='w') as f:
@@ -230,9 +239,6 @@ class PointCloud:
                 f.user_data = points['user_data'].values
             if 'intensity' in points:
                 f.intensity = points['intensity'].values
-        if filename == 'TEMPORARY.las':
-            self.__zip_las('TEMPORARY.las', orig_filename)
-            os.system('rm TEMPORARY.las')
 
     def __write_xyz_file(self, filename, points):
         points.to_csv(filename)
